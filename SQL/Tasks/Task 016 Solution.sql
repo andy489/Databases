d9b35dc31a76f4792db362e63bf935e127d241cf
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-24 22:37:19 +0200

    Create Task 016 Solution.sql

---
+++ SQL/Tasks/Task 016 Solution.sql
@@ -0,0 +1,13 @@
+/*
+MySQL
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE lower(CITY) regexp '[^aeiou]$';
+
+/*
+Oracle
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE regexp_like(lower(CITY),'[^aeiou]$');
