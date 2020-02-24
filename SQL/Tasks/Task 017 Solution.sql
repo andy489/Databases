bfa5828b9f50400318cd298754cc6244585c004f
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-24 23:17:29 +0200

    Create Task 017 Solution.sql

---
+++ SQL/Tasks/Task 017 Solution.sql
@@ -0,0 +1,13 @@
+/*
+MySql
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE lower(CITY) regexp '^[^aeiou].*|.*[^aeiou]$';
+
+/*
+Oracle
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE regexp_like(lower(CITY),'^[^aeiou].*|.*[^aeiou]$');
