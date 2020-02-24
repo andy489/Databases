1c7992234b2ed427943959d1c2fdfd28f3e3ddc8
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-24 20:31:42 +0200

    Update Task 012 Solution.sql

--- SQL/Tasks/Task 012 Solution.sql
+++ SQL/Tasks/Task 012 Solution.sql
@@ -1,17 +1,25 @@
 /*
 MySQL
 */
-SELECT CITY
-FROM STATION
+SELECT DISTINCT CITY 
+FROM STATION 
 WHERE CITY regexp "^[aeiou].*";
 
 /*
-Oracle & MySQL
+Oracle & MySQL & MS SLQ Server & DB2
 */
-SELECT CITY
-FROM STATION
+SELECT DISTINCT CITY 
+FROM STATION 
 WHERE lower(CITY) like 'a%'
       or lower(CITY) like 'e%'
       or lower(CITY) like 'i%'
       or lower(CITY) like 'o%'
       or lower(CITY) like 'u%';
+      
+/*
+Oracle & MySQL & DB2
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE substr(lower(CITY),1,1) 
+      in ('a','e','i','o','u');
