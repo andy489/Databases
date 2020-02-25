04cea5056ceed2228b3c41a1ca363def71cce3b0
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-25 23:39:14 +0200

    Update Task 030 Solution.sql

--- SQL/Tasks/Task 030 Solution.sql
+++ SQL/Tasks/Task 030 Solution.sql
@@ -1,4 +1,14 @@
 /*
+mySQL & Oracle & MS SQL Server & DB2
+*/
+SELECT months*salary, count(*) 
+FROM Employee
+WHERE months*salary = (SELECT max(months*salary) 
+                  FROM Employee)
+GROUP BY months*salary;
+
+
+/*
 MySQL
 */
 SELECT (SALARY * MONTHS) AS EARNINGS, count(*) 
