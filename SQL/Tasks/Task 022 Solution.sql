414f0b6db19c9a93ace4a1593d946e0d2a13ea49
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-25 21:11:11 +0200

    Create Task 022 Solution.sql

---
+++ SQL/Tasks/Task 022 Solution.sql
@@ -0,0 +1,14 @@
+/*
+MySQL & Oracle & MS SQL Server & DB2
+*/
+SELECT CASE 
+WHEN A+B>C AND A+C>B AND B+C>A 
+     THEN CASE 
+     WHEN A=B AND B=C 
+          THEN 'Equilateral' 
+          WHEN A=B OR B=C OR A=C 
+               THEN 'Isosceles' 
+               ELSE 'Scalene' END 
+ELSE 'Not A Triangle' END 
+FROM TRIANGLES;
+
