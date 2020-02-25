c7e0a0c0b72c0d7c7d936e944a5ffc0b5e634c4a
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-25 21:44:59 +0200

    Create Task 026 Average Population.sql

---
+++ SQL/Tasks/Task 026 Average Population.sql
@@ -0,0 +1,11 @@
+## Average Population
+
+[Judge System](https://www.hackerrank.com/challenges/average-population/problem)
+
+Query the average population for all cities in **CITY**, rounded down to the nearest integer.
+
+#### Input Format
+
+The **CITY** table is described as follows:
+
+![](https://github.com/andy489/Database/blob/master/assets/Revising%20the%20Select%20Query%20I.jpg)
