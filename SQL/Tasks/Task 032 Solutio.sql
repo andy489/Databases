d1d40cf5d8537e71904c81f9e37a6f8e51100e17
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-26 10:59:51 +0200

    Create Task 032 Solutio.sql

---
+++ SQL/Tasks/Task 032 Solutio.sql
@@ -0,0 +1,13 @@
+## Weather Observation Station 15
+
+[Judge System](https://www.hackerrank.com/challenges/weather-observation-station-15/problem)
+
+Query the greatest value of the Northern Latitudes (LAT_N) from **STATION** that is less than <img src="https://latex.codecogs.com/svg.latex?\Large&space;137.2345">. Truncate your answer to <img src="https://latex.codecogs.com/svg.latex?\Large&space;4"> decimal places.
+
+#### Input Format
+
+The **STATION** table is described as follows:
+
+![](https://github.com/andy489/Database/blob/master/assets/Weather%20Observation%20Station%201.jpg)
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
