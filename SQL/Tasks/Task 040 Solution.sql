bd1cafe2120d064629101078f7789bfa773e04b5
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-26 18:14:19 +0200

    Create Task 040 Solution.sql

---
+++ SQL/Tasks/Task 040 Solution.sql
@@ -0,0 +1,23 @@
+/*
+MS SQL Server
+*/
+DECLARE @var smallint = 1;
+
+WHILE @var < 21
+BEGIN
+    SELECT REPLICATE('* ', @var);
+    SET @var += 1;
+END;
+
+/*
+MySQL
+*/
+SELECT REPEAT('* ', @NUMBER := @NUMBER + 1) 
+FROM information_schema.tables, (SELECT @NUMBER:=0) t LIMIT 20;
+
+/*
+Oracle
+*/
+SELECT rpad('* ', (LEVEL)*2, '* ')
+FROM dual
+CONNECT BY LEVEL < 21;
