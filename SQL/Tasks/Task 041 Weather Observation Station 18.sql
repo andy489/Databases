1608031c98f127227b4690d9e6c996264eeed03b
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-26 18:26:34 +0200

    Create Task 041 Weather Observation Station 18.sql

---
+++ SQL/Tasks/Task 041 Weather Observation Station 18.sql
@@ -0,0 +1,22 @@
+Consider <img src="https://latex.codecogs.com/svg.latex?\Large&space;P_1(a,b)"> and <img src="https://latex.codecogs.com/svg.latex?\Large&space;P_2(c,d)">
+
+to be two points on a 2D plane.
+
+happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+
+    happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+
+Query the Manhattan Distance between points
+and and round it to a scale of
+
+decimal places.
+
+#### Input Format
+
+The STATION table is described as follows:
+
+![](https://github.com/andy489/Database/blob/master/assets/Weather%20Observation%20Station%201.jpg)
+
+where LAT_N is the northern latitude and LONG_W is the western longitude. 
