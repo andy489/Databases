4ff0f5fb3b1e6111993841df9597073a9d38112b
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-26 11:41:33 +0200

    Update Task 017 Solution.sql

--- SQL/Tasks/Task 017 Solution.sql
+++ SQL/Tasks/Task 017 Solution.sql
@@ -1,5 +1,13 @@
 /*
-MySql
+MS SQL Server
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE lower(CITY) NOT LIKE '%[a,e,i,o,u]'
+           OR lower(CITY) NOT LIKE '[a,e,i,o,u]%';
+
+/*
+MySQL
 */
 SELECT DISTINCT CITY
 FROM STATION
