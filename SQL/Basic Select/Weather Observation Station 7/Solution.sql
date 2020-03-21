57bb3244139f8a9a547572b646d1c534bf071af8
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-22 00:02:55 +0200

    Create Solution.sql

---
+++ SQL/Basic Select/Weather Observation Station 7/Solution.sql
@@ -0,0 +1,35 @@
+-- github.com/andy489
+
+-- MS SQL Server
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE '%[aeiou]';
+
+-- Oracle
+SELECT DISTINCT CITY
+FROM STATION 
+WHERE regexp_like(CITY,'[aeiou]$');
+
+-- MySQL, Oracle
+SELECT DISTINCT CITY
+FROM STATION 
+WHERE lower(substr(CITY,length(CITY),1))
+      in ('a','e','i','o','u');
+
+-- MySQL
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY REGEXP '[aeiou]$';
+
+--  Pattern   | What the pattern matches
+---------------------------------------
+--    $       |  End of string
+--  [...]     |  Any character listed between the square brackets
+
+-- Useful Reference: https://www.tutorialspoint.com/mysql/mysql-regexps.htm
+
+-- OR
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE substr(CITY,-1) 
+      in ('a','e','o','i','u');
