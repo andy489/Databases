fc3d10475aae788ab053a46f04ace6ad60511c28
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-30 23:57:19 +0300

    Update 06. Summary Solutions.sql

--- SQL/06. Summary Solutions.sql
+++ SQL/06. Summary Solutions.sql
@@ -31,3 +31,28 @@ from (select studioname, max(year) as year
 	  inner join movie as t1 on t1.studioname = t2.studioname 
 	  and t1.year = t2.year
 order by 1 desc;
+
+-- 6 
+select name
+from moviestar
+where gender = 'M'
+order by birthdate desc limit 1;
+
+-- 7
+select t3.studioname, t3.starname, t3.num_movies 
+from (select studioname, starname, count(title) as num_movies 
+	  from movie right join starsin on movietitle = title 
+	  group by studioname, starname) t3 
+	  inner join 
+	  (select studioname, max(num_movies) as num_movies 
+	  from (select studioname, starname, count(title) as num_movies 
+	  from movie right join starsin on movietitle = title 
+	  group by studioname, starname) t1 group by studioname) t2 
+	  on t2.studioname = t3.studioname and t2.num_movies = t3.num_movies;
+	  
+-- 8
+select t1.title, movie.year, count as num_actors
+from(select title, count(starname) as count
+from movie join starsin on movietitle = title 
+group by title having count(starname)>2) t1 
+join movie on t1.title = movie.title;
