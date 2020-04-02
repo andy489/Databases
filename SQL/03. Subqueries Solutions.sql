6d08aa457ff4b1fdd69d7193ab6727a787da2515
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-02 16:36:10 +0300

    Update 03. Subqueries Solutions.sql

--- SQL/03. Subqueries Solutions.sql
+++ SQL/03. Subqueries Solutions.sql
@@ -1,21 +1,21 @@
 -- github.com/andy489
 
---MOVIES
---1
+-- MOVIES
+-- 1
 select name
 from moviestar
 where gender ='F' and name in (select name
 			       from movieexec
 			       where networth > 10000000);
 
---2
+-- 2
 select name
 from moviestar
 where name not in (select name 
 		   from movieexec)
 order by name;
 
---3
+-- 3
 select title
 from movie
 where length > (select length
@@ -23,8 +23,8 @@ where length > (select length
 		where title = 'Star Wars')
 order by title;
 
---4
---първи начин
+-- 4
+-- first
 select title, movieexec.name
 from movie
 join movieexec on movieexec."CERT#" = movie."PRODUCERC#"
@@ -35,7 +35,7 @@ where "PRODUCERC#" in (select "CERT#"
 				 	 where name = 'Merv Griffin'))
 order by title;
 
---втори начин
+-- second
 select movie.title, ex.name
 from movie, (select "CERT#", name
              from movieexec
@@ -44,27 +44,27 @@ from movie, (select "CERT#", name
 			       where name = 'Merv Griffin')) ex
 where "PRODUCERC#" = "CERT#" order by title;
 
---PC
---1
+-- PC
+-- 1
 select maker 
 from product
 where model in (select model
 		from pc
 		where speed > 500);
 
---2
+-- 2
 select code, model, price
 from printer
 where price >= all(select price 
 		   from printer);
 			 
---3
+-- 3
 select *
 from laptop
 where speed < all(select speed
 		  from pc);
 
---4
+-- 4
 select model, price 
 from laptop 
 where price >= all(select price from laptop 
@@ -84,7 +84,7 @@ where price >= all(select price from laptop
 				union select price from pc 
 				union select price from printer);
 
---5
+-- 5
 select maker	
 from product	
 where model in (select model
@@ -94,27 +94,27 @@ where model in (select model
 				 from printer
 				 where color = 'y'))
 
---6 
+-- 6 
 select maker
 from product p, (select ram, speed, model from pc)t
 where t.model = p.model and t.ram <=all(select ram from pc)
 and t.speed>=all(select speed from pc where t.ram = ram)
 
---SHIPS
---1
+-- SHIPS
+-- 1
 select distinct country
 from classes
 where numguns >=all(select numguns 
 		    from classes);
 
---2
+-- 2
 select distinct class 
 from ships
 where name in (	select ship
 		from outcomes
 		where result ='sunk');
 
---3
+-- 3
 select name, class
 from ships
 where class in (select "class" 
@@ -122,14 +122,14 @@ where class in (select "class"
 		where bore = '16')
 order by name;
 
---4
+-- 4
 select battle
 from outcomes
 where ship in (select name
 		from ships
 		where class = 'Kongo');
 
---5
+-- 5
 select class, name
 from ships
 where class in (select "class"
